f4bb5b793fd12b05cb7f4cc44460ff3c75fb64e4
Niko Ross <niko.bear.ross@gmail.com>
2024-10-28 01:01:58 -0400

    created pages and all basic style, removed unecessary code

--- client/src/components/MainPage.js
+++ client/src/components/MainPage.js
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './MainPage.css';
 
 const MainPage = () => {
   const navigate = useNavigate();
+  const [sidebarVisible, setSidebarVisible] = useState(false);
+
+  const toggleSidebar = () => {
+    setSidebarVisible(!sidebarVisible);
+  };
 
   return (
     <div className="main-page">
-      <button className="custom-button" onClick={() => navigate('/host')}>Host Game</button>
-      <button className="custom-button" onClick={() => navigate('/join')}>Join Game</button>
+      <div className="top-bar">
+        <div className="hamburger" onClick={toggleSidebar}>&#9776;</div>
+        <div className="name-logo">Guessify</div>
+      </div>
+      <div className={`sidebar ${sidebarVisible ? 'visible' : ''}`}>
+      <div className="sidebar-content">
+        <div className="logo"></div> {/* Logo placeholder */}
+        <div className="close-button" onClick={toggleSidebar}>&times;</div>
+        <p>Menu Item 1</p>
+        <p>Menu Item 2</p>
+        <p>Menu Item 3</p>
+        </div>
+      </div>
+      <div className="content">
+        <button className="custom-button" onClick={() => navigate('/host')}>Host Game</button>
+        <button className="custom-button" onClick={() => navigate('/join')}>Join Game</button>
+      </div>
     </div>
   );
 };
