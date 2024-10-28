0087c117f3fc890035a8a85599a053d3eaad14f1
Niko Ross <niko.bear.ross@gmail.com>
2024-10-28 01:46:22 -0400

    messing with virtual enviroments

---
+++ flask-server/db.py
@@ -0,0 +1,34 @@
+from flask_sqlalchemy import SQLAlchemy
+
+# Initialize the database
+db = SQLAlchemy()
+
+# Define the User model
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(150), nullable=False)
+    spotify_token = db.Column(db.String(500), nullable=True)
+    spotify_refresh_token = db.Column(db.String(500), nullable=True)
+
+# Function to initialize the database
+def init_db(app):
+    db.init_app(app)
+    with app.app_context():
+        db.create_all()
+
+# Function to add a new user
+def add_user(email, password):
+    new_user = User(email=email, password=password)
+    db.session.add(new_user)
+    db.session.commit()
+
+# Function to get a user by email
+def get_user_by_email(email):
+    return User.query.filter_by(email=email).first()
+
+# Function to update Spotify tokens for a user
+def update_spotify_tokens(user, access_token, refresh_token):
+    user.spotify_token = access_token
+    user.spotify_refresh_token = refresh_token
+    db.session.commit()
--- flask-server/server.py
+++ flask-server/server.py
@@ -5,4 +5,21 @@ Info to look into before building:
     1. How to play songs (can we play songs from Spotify on a phone?)
     2. Database information for production (duckdb works locally but 
        don't know if it will work in a production environment)
+
+
+This to remeber:
+    1. Create a requirements.txt file
+
+Routes:
+    1. /login
+    2. /signup
+    3. /play
+    4. /pause
+    5. /skip
+    6. /guess
+    7. /leaderboard
+    8. /profile
+    9. /update_profile
+    10. /logout
+    11. /refresh_token
 """
